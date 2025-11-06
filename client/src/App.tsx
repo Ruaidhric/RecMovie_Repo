42226e3d02ff07c1d188c77d2667c6471aaa4b6d
Renata Balan <andrea.balan@mycit.ie>
2025-11-06 12:12:33 +0000

    feat(preference-form):Implemented preference form UI design into React code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import {Toaster} from "@/components/ui/sonner";
 import Login from "./pages/Login";
+import {Preferences} from "./pages/Preferences";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/preferences" element={<Preferences />} />
+
       </Routes>
     </>
   );
